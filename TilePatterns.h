99ff024bdd3380d9110e143ada8f2ede5c7e81c7
André Baptista <albs_br@yahoo.com.br>
2021-07-26 20:09:34 -0300

    v.0.8.0

--- TilePatterns.h
+++ TilePatterns.h
@@ -12,6 +12,8 @@ static const unsigned char pattern_black[] = {
     0b11111111,
   };
 
+// ---------------------------------------------
+
 // top left
 static const unsigned char pattern_0[] = {
     0b10011111,
@@ -50,6 +52,58 @@ static const unsigned char pattern_2[] = {
 
 // bottom right
 static const unsigned char pattern_3[] = {
+    0b01001001,
+    0b11001001,
+    0b00001001,
+    0b00001001,
+    0b11111001,
+    0b00000001,
+    0b00000001,
+    0b11111111,
+  };
+
+// ---------------------------------------------
+
+// top left
+static const unsigned char pattern_4[] = {
+    0b01111111,
+    0b10000001,
+    0b10000011,
+    0b10000111,
+    0b10001111,
+    0b10011111,
+    0b10111111,
+    0b11111111,
+  };
+
+// TODO:
+
+// top right
+static const unsigned char pattern_5[] = {
+    0b11111111,
+    0b00000001,
+    0b00000001,
+    0b11111001,
+    0b00001001,
+    0b00001001,
+    0b01001001,
+    0b01001001,
+  };
+
+// top left
+static const unsigned char pattern_6[] = {
+    0b10010010,
+    0b10010011,
+    0b10010000,
+    0b10010000,
+    0b10011111,
+    0b10000000,
+    0b10000000,
+    0b11111111,
+  };
+
+// bottom right
+static const unsigned char pattern_7[] = {
     0b01001001,
     0b11001001,
     0b00001001,
